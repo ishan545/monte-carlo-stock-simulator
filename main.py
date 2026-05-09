14f55ed2344914e3049f71eda36e7d1190f7fe5c
Ishan <135945323+ishan545@users.noreply.github.com>
2026-05-09 12:45:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Monte Carlo Stock Simulator - Main Entry Point
+
+A GUI application for running Monte Carlo simulations on stock prices
+"""
+
+from gui import main
+
+if __name__ == "__main__":
+    main()
